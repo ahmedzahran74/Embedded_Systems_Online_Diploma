decf21f2b207e86b1b050ac99498a21306bef5c3
ahmedzahran74 <azahran774@gmail.com>
2022-06-29 17:56:08 +0200

    get string

---
+++ C/extra/get_string from user with space.c
@@ -0,0 +1,30 @@
+/*
+ * test.c
+ *
+ *  Created on: Jun 29, 2022
+ *      Author: AM
+ */
+
+
+#include "stdio.h"
+#define N 5
+void fun (char *str,int n)
+{
+	char ch;
+	for (int i=0;i<n;i++)
+	{
+		if ((ch=getchar())!='\n')
+			str[i]=ch;
+	}
+	str[N]='\0';
+
+}
+void main (int argc,char **argv)
+{
+	char s[N+1];
+	fun(s,N);
+	printf("%s",s);
+	fflush(stdin);fflush(stdout);
+
+}
+
