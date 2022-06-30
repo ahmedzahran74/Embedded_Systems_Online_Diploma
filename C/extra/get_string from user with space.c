ee2dd7731f6a280797f6813a872ba3415ea55774
ahmedzahran74 <azahran774@gmail.com>
2022-06-30 19:41:26 +0200

    \n modification

--- C/extra/get_string from user with space.c
+++ C/extra/get_string from user with space.c
@@ -11,11 +11,19 @@
 void fun (char *str,int n)
 {
 	char ch;
+	char flag=0;
 	for (int i=0;i<n;i++)
 	{
 		if ((ch=getchar())!='\n')
 			str[i]=ch;
+		else
+		{
+			str[i]=0;
+			break;
+			flag=1;
+		}
 	}
+	if (flag==0)
 	str[N]='\0';
 
 }
