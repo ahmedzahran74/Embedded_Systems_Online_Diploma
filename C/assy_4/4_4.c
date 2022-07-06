c3e4eb03e5cd08751c692a7a6cda6de9ad4de08c
ahmedzahran74 <azahran774@gmail.com>
2022-07-06 18:26:16 +0200

    4_4

--- C/assy_4/4_4.c
+++ C/assy_4/4_4.c
@@ -11,7 +11,7 @@ typedef struct{
 	char name[30];
 	int roll;
 	float marks;
-}employee;
+}student;
 void getting_string(char *str,int n)
 {
 	char ch;
@@ -31,59 +31,59 @@ void getting_string(char *str,int n)
 	str[n]='\0';
 
 }
-void get_employee_data(employee*emp,int i)
+void get_student_data(student*stu,int i)
 {
 
 	char first_name[14];
 	char second_name[14];
-	printf("Enter the first name of employee %d: ",i);
+	printf("Enter the first name of student %d: ",i);
 	fflush(stdin);fflush(stdout);
 	getting_string(first_name,10);
-	printf("Enter the second name of employee %d: ",i);
+	printf("Enter the second name of student %d: ",i);
 	fflush(stdin);fflush(stdout);
 	getting_string(second_name,10);
-	strcpy(emp->name,first_name);
-	strcat(emp->name,"  ");
-	strcat(emp->name,second_name);
+	strcpy(stu->name,first_name);
+	strcat(stu->name,"  ");
+	strcat(stu->name,second_name);
 	fflush(stdin);fflush(stdout);
-	printf("Enter roll number of employee %d: ",i);
+	printf("Enter roll number of student %d: ",i);
 	fflush(stdin);fflush(stdout);
-	scanf("%d",&emp->roll);
+	scanf("%d",&stu->roll);
 	printf("Enter the marks of employee %d: ",i);
 	fflush(stdin);fflush(stdout);
-	scanf("%f",&emp->marks);
+	scanf("%f",&stu->marks);
 }
-void get_employees_data(employee*emp,int n)
+void get_students_data(student*emp,int n)
 {
 	for (int i=1;i<=n;i++)
 	{
-		get_employee_data(emp+i-1,i);
+		get_student_data(emp+i-1,i);
 	}
 }
-void print_employee_data(employee*emp,int i)
+void print_student_data(student*stu,int i)
 {
-	printf("The name of employee %d is: %s\n",i,emp->name);
+	printf("The name of employee %d is: %s\n",i,stu->name);
 	fflush(stdin);fflush(stdout);
-	printf("The roll number of employee %d is %d\n",i,emp->roll);
+	printf("The roll number of employee %d is %d\n",i,stu->roll);
 	fflush(stdin);fflush(stdout);
-	printf("The marks of employee %d is %f\n",i,emp->marks);
+	printf("The marks of employee %d is %.2f\n",i,stu->marks);
 	fflush(stdin);fflush(stdout);
 }
-void print_employees_data(employee*emp,int n)
+void print_students_data(student*stu,int n)
 {
 	for (int i=1;i<=n;i++)
 	{
-		print_employee_data(emp+i-1,i);
+		print_student_data(stu+i-1,i);
 	}
 }
 int main()
 {
-	employee emps[N];
+	student stus[N];
 	int index;
-	get_employees_data(emps,N);
-	//print_employees_data(emps,N);
-	printf("Enter the employee number you want to know about: ");
+	get_students_data(stus,N);
+	//print_employees_data(stu,N);
+	printf("Enter the student number you want to know about: ");
 	fflush(stdin);fflush(stdout);
 	scanf("%d",&index);
-	print_employee_data(&emps[index-1],index);
+	print_student_data(&stus[index-1],index);
 }
