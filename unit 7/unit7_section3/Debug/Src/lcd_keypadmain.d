ffbd0210d62e3b892cf78e352b734b18f8605ea9
ahmedzahran74 <87994551+ahmedzahran74@users.noreply.github.com>
2022-08-29 19:59:31 +0200

    lcd and keypad drivers

---
+++ unit 7/unit7_section3/Debug/Src/lcd_keypadmain.d
@@ -0,0 +1,19 @@
+Src/lcd_keypadmain.o: ../Src/lcd_keypadmain.c \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/HAL/Inc/LCD.h \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6.h \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6_GPIO.h \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6.h \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/useful_macros.h \
+ C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/HAL/Inc/Keypad.h
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/HAL/Inc/LCD.h:
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6.h:
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6_GPIO.h:
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/STM32f103c6.h:
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/STM32f103c6_Drivers/Inc/useful_macros.h:
+
+C:/Users/AM/STM32CubeIDE/workspace_1.4.0/unit7_lesson3/HAL/Inc/Keypad.h:
---
+++ unit 7/unit7_section3/HAL/Src/Keypad.c
@@ -0,0 +1,115 @@
+/*
+ * Keypad.c
+ *
+ *  Created on: Aug 29, 2022
+ *      Author: AM
+ */
+
+#include"keypad.h"
+
+static GPIO_pin_configue_t PinCfg;
+
+int KEYPAD_R[]={R0,R1,R2,R3};
+int KEYPAD_C[]={C0,C1,C2,C3};
+
+void Keypad_init()
+{
+	PinCfg.GPIO_pin_number = R0;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_pin_number = R1;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+
+	PinCfg.GPIO_pin_number = R2;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_pin_number = R3;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+
+	PinCfg.GPIO_pin_number = C0;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_pin_number = C1;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_pin_number = C2;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_pin_number = C3;
+	PinCfg.GPIO_pin_mode = GPIO_MODE_OUTPUT_10M ;
+	PinCfg.GPIO_pin_CNF = GPIO_OUT_PUSHPULL ;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+	MCAL_GPIO_Write_port(KEYPAD_PORT, 0xFF);
+}
+
+char Keypad_getkey()
+{
+	volatile int i, j;
+	for (i=0;i<4;i++)
+	{
+
+		MCAL_GPIO_Write_Pin(KEYPAD_PORT, KEYPAD_C[0], HIGH);
+		MCAL_GPIO_Write_Pin(KEYPAD_PORT, KEYPAD_C[1], HIGH);
+		MCAL_GPIO_Write_Pin(KEYPAD_PORT, KEYPAD_C[2], HIGH);
+		MCAL_GPIO_Write_Pin(KEYPAD_PORT, KEYPAD_C[3], HIGH);
+		MCAL_GPIO_Write_Pin(KEYPAD_PORT, KEYPAD_C[i],LOW);
+
+		for(j=0;j<4;j++)
+		{
+
+			if((MCAL_GPIO_Read_Pin(KEYPAD_PORT, KEYPAD_R[j]))==0)
+			{
+				while((MCAL_GPIO_Read_Pin(KEYPAD_PORT, KEYPAD_R[j]))==0);
+				switch(i)
+				{
+				case 0:
+				{
+					if(j==0) return '7';
+					else if (j==1) return '4';
+					else if (j==2) return '1';
+					else if (j==3) return '!';
+				}break;
+				case 1:
+				{
+					if(j==0) return '8';
+					else if (j==1) return '5';
+					else if (j==2) return '2';
+					else if (j==3) return '0';
+				}break;
+				case 2:
+				{
+					if(j==0) return '9';
+					else if (j==1) return '6';
+					else if (j==2) return '3';
+					else if (j==3) return '=';
+				}break;
+				case 3:
+				{
+					if(j==0) return '/';
+					else if (j==1) return '*';
+					else if (j==2) return '-';
+					else if (j==3) return '+';
+				}break;
+				}
+			}
+		}
+
+	}
+	return 'N';
+}
